43edcc537a7b7980736ea9e09e61bf6db77a8e28
Anthony De La Cruz <adelacruz@connexco.com.do>
2022-11-14 12:41:09 +0000

    [ADD] Basic Module

---
+++ odoo_space_mission/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Space Mission',
+    
+    'summary': """ Project to handled a Space Mission """,
+    
+    'description': """ 
+        Space Mission project to provide the necessary resources 
+            -Inventory
+            -Contacts Manage
+            -Training
+    """,
+    
+    'author': 'Anthony De La Cruz',
+    
+    'website': 'https://www.connexco.com.do',
+    
+    'category': 'space',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data':[
+        
+    ],
+    
+    'demo':[
+        
+    ],
+}
